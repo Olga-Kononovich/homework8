16ed2e105f72f0e36c95d2c6784b12eb8333e557
Olga-Kononovich <o.chesna@gmail.com>
2025-04-07 18:46:18 +0300

    дз 8

--- src/Main.java
+++ src/Main.java
@@ -39,6 +39,7 @@ public class Main {
             System.out.println(pannier1[a]);
         }
 
+
         int[] pannier2 = {10, 11, 13, 14};
         for (int i = pannier2.length - 1; i >= 0; i--) {
             System.out.println(pannier2[i]);
